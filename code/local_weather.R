f2394f445e06d73ec7a0525950cf8c63bd1dedd1
Pat Schloss <pschloss@umich.edu>
2022-07-18 12:27:55 -0400

    Data cleaning episode

--- code/local_weather.R
+++ code/local_weather.R
@@ -5,9 +5,11 @@ library(lubridate)
 inventory_url <- "https://www.ncei.noaa.gov/pub/data/ghcn/daily/ghcnd-inventory.txt"
 
 inventory <- read_table(inventory_url,
-                        col_names = c("station", "lat", "lon", "variable", "start", "end"))
+                        col_names = c("station", "lat", "lon",
+                                      "variable", "start", "end"))
 
 
+#Coordinates for Dexter, MI
 my_lat <- 42.33831964441621 * 2 * pi / 360
 my_lon <- -83.88938389977316 * 2 * pi / 360
 
@@ -20,7 +22,9 @@ my_lon <- -83.88938389977316 * 2 * pi / 360
 my_station <- inventory %>%
   mutate(lat_r = lat *2 *pi/360,
          lon_r = lon *2 *pi/360,
-         d = 1.609344 * 3963 * acos((sin(lat_r) * sin(my_lat)) + cos(lat_r) * cos(my_lat) * cos(my_lon - lon_r))
+         d = 1.609344 * 3963 * acos((sin(lat_r) * sin(my_lat)) +
+                                      cos(lat_r) * cos(my_lat) *
+                                      cos(my_lon - lon_r))
          ) %>%
   filter(start < 1960 & end > 2020) %>%
   top_n(n = -1, d) %>%
@@ -30,9 +34,53 @@ my_station <- inventory %>%
 station_daily <- glue("https://www.ncei.noaa.gov/pub/data/ghcn/daily/by_station/{my_station}.csv.gz")
 
 local_weather <- read_csv(station_daily,
-         col_names = c("station", "date", "variable", "value", "a", "b", "c", "d")) %>%
+         col_names = c("station", "date", "variable", "value",
+                       "a", "b", "c", "d")) %>%
   select(date, variable, value) %>%
   pivot_wider(names_from = "variable", values_from="value",
               values_fill = 0) %>%
   select(date, TMAX, PRCP, SNOW) %>%
-  mutate(date = ymd(date))
+  mutate(date = ymd(date),
+         TMAX = TMAX / 10,
+         PRCP = PRCP / 10) %>%
+  rename_all(tolower) %>%
+  # filter(prcp < 200) %>%
+  mutate(snow = if_else(snow < 500, snow, NA_real_),
+         prcp = if_else(prcp < 200, prcp, NA_real_),
+         date = na_if(date, "1891-10-09"),
+         date = if_else(date == "1891-10-05", as.Date(NA_character_), date)) %>%
+  drop_na(prcp)
+
+
+local_weather %>%
+  ggplot(aes(x=date, y=tmax)) +
+  geom_line()
+
+local_weather %>%
+  ggplot(aes(x = tmax)) +
+  geom_histogram(binwidth = 2.5)
+
+local_weather %>%
+  ggplot(aes(x=date, y=prcp)) +
+  geom_line()
+
+local_weather %>%
+  slice_max(n=5, prcp)
+
+local_weather %>%
+  ggplot(aes(x = prcp)) +
+  geom_histogram() +
+  scale_y_continuous(limits = c(0, 50))
+
+
+local_weather %>%
+  ggplot(aes(x=date, y=snow)) +
+  geom_line()
+
+local_weather %>%
+  slice_max(n=5, snow)
+
+local_weather %>%
+  ggplot(aes(x = snow)) +
+  geom_histogram() +
+  scale_y_continuous(limits = c(0, 50))
