113eef3856d1ce2208a966db4439b71f4ea9e580
Pat Schloss <pschloss@umich.edu>
2022-07-26 10:25:35 -0400

    Fix bugs I noticed in editing video

--- code/culumulative_prcp_plot.R
+++ code/culumulative_prcp_plot.R
@@ -14,23 +14,24 @@ local_weather %>%
          month = month(date),
          day = day(date),
          is_this_year = year == this_year) %>%
-  filter(year != 1891 & (month != 2 & day != 29)) %>%
+  filter(year != 1891 & !(month == 2 & day == 29)) %>%
+  arrange(date) %>%
   group_by(year) %>%
   mutate(cum_prcp = cumsum(prcp)) %>%
   ungroup() %>%
   mutate(new_date = ymd(glue("2022-{month}-{day}"))) %>%
   ggplot(aes(x = new_date, y =cum_prcp, group = year,
              color=is_this_year, size=is_this_year)) +
-  geom_line(show.legend = FALSE) +
+  geom_step(show.legend = FALSE) +
   geom_smooth(aes(group = 1), color = "black", size = 0.3, se= FALSE) +
   scale_color_manual(breaks = c(F, T),
                      values = c("lightgray", "dodgerblue")) +
   scale_size_manual(breaks = c(F, T),
                     values = c(0.3, 1)) +
   scale_x_date(date_labels = "%B", date_breaks="2 months") +
-  scale_y_continuous(breaks = seq(0, 1200, 300),
-                     labels = seq(0, 120, 30),
-                     limits = c(0, 1200),
+  scale_y_continuous(breaks = seq(0, 1500, 300),
+                     labels = seq(0, 150, 30),
+                     limits = c(0, 1500),
                      expand= c(0,0)) +
   labs(x = NULL,
        y = "Cumulative precipitation (cm)",
