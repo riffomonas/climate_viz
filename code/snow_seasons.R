df5baf7e7de84de7bfbf468296067e3c00ccafac
Pat Schloss <pschloss@umich.edu>
2022-08-05 16:29:35 -0400

    Snow year snow episode

--- code/snow_seasons.R
+++ code/snow_seasons.R
@@ -1,29 +1,68 @@
+library(ggtext)
+
 source("code/local_weather.R")
 
-local_weather %>%
+snow_data <- local_weather %>%
   select(date, snow) %>%
-  mutate(snow = if_else(is.na(snow), 0, snow),
-         year = year(date),
+  drop_na(snow) %>%
+  mutate(cal_year = year(date),
          month = month(date),
-         snow_year = if_else(date < ymd(glue("{year}-08-01")),
-                             year - 1,
-                             year)) %>% 
-  filter(snow_year != 1891 & snow_year != 1955) %>%
-  group_by(month, snow_year) %>%
+         snow_year = if_else(date < ymd(glue("{cal_year}-07-01")),
+                             cal_year - 1,
+                             cal_year)) %>%
+  select(month, snow_year, snow) %>%
+  filter(snow_year != 1891 & snow_year != 2022)
+
+
+snow_data %>%
+  group_by(snow_year) %>%
+  summarize(total_snow = sum(snow)) %>%
+  ggplot(aes(x = snow_year, y = total_snow)) +
+  geom_line()
+
+snow_data %>%
+  filter(snow > 0) %>%
+  count(snow_year) %>%
+  ggplot(aes(x = snow_year, y = n)) +
+  geom_line()
+
+
+dummy_df <- crossing(snow_year = 1892:2021,
+                     month = 1:12) %>%
+  mutate(dummy = 0)
+
+total_snow <- snow_data %>%
+  group_by(snow_year) %>%
+  summarize(total_snow = sum(snow)) %>%
+  filter(snow_year == 2021) %>%
+  mutate(total_snow = total_snow/10) %>%
+  pull(total_snow)
+
+
+snow_data %>%
+  right_join(., dummy_df, by = c("snow_year", "month")) %>%
+  mutate(snow  = if_else(is.na(snow), dummy, snow )) %>%
+  group_by(snow_year, month) %>%
   summarize(snow = sum(snow), .groups = "drop") %>%
   mutate(month = factor(month, levels = c(8:12, 1:7)),
-         is_this_snow_year = snow_year == 2021) %>%
-  ggplot(aes(x = month, y = snow, group = snow_year,
-             color = is_this_snow_year, size = is_this_snow_year)) +
-  geom_line() +
+         is_this_year = 2021 == snow_year) %>%
+  ggplot(aes(x = month, y = snow, group = snow_year, color = is_this_year)) +
+  geom_line(show.legend = FALSE) +
+  scale_color_manual(name = NULL,
+                     breaks = c(T, F),
+                     values = c("dodgerblue", "gray")
+                     ) +
   scale_x_discrete(breaks = c(9, 11, 1, 3, 5),
-                   labels = month.name[c(9, 11, 1, 3, 5)],
-                   expand = c(0,0)) +
-  scale_color_manual(breaks = c(F, T),
-                     values = c("lightgray", "dodgerblue"), guide = "none") +
-  scale_size_manual(breaks = c(F, T),
-                    values = c(0.25, 1), guide = "none") +
-  labs(x = NULL, y = "Precipitation by month for each snow season (mm)") +
-  theme_classic()
+                   labels = month.abb[c(9, 11, 1, 3, 5)],
+                   expand = c(0, 0 )) +
+  scale_y_continuous(breaks = seq(0, 1500, 500),
+                     labels = seq(0, 150, 50)) +
+  labs(x = NULL, y = "Total monthly snowfall (cm)",
+       title = glue("The <span style = 'color:dodgerblue'>snow year 2021</span> had a total of {total_snow} cm of snow")) +
+  theme(panel.background = element_blank(),
+        panel.grid = element_blank(),
+        axis.line = element_line(),
+        plot.title.position = "plot",
+        plot.title = element_markdown())
 
-ggsave("figures/snow_by_snow_year.png", width=6, height =4)
+ggsave("figures/snow_by_snow_year.png", width = 6, height = 4)
